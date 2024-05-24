922bb77d5e7c42d0ddf307e74e1cb071eb4655c0
yvette rodeo <yrodeo@yearup.org>
2024-05-23 23:27:21 -0500

    updated my website

--- scripts/mountains.js
+++ scripts/mountains.js
@@ -1,16 +1,108 @@
 "use strict"
 
-
 window.onload = () => {
 
-    /*
-    You can remove the following console.log() lines.
-    They are here to verify that we have access to the data
-    The data script files are located in the scripts/data directory
-*/
+    initMountainSelect();
 
-    //log the mountainsArray to the console (scripts/data/mountainData.js)
-    console.log(mountainsArray)
+    let mountainSelection = document.querySelector("#mountainInfo");
+
+    mountainSelection.addEventListener("change", displayMountainCard)
+
+
+
+}
+function initMountainSelect() {
+
+    let mountainSelection = document.querySelector("#mountainInfo");
+
+    let defaultOption = document.createElement("option");
+    defaultOption.value = "";
+    defaultOption.textContent = "--Select A Mountain--";
+
+    mountainSelection.appendChild(defaultOption);
+
+
+    mountainsArray.forEach((name) => {
+
+       
+        let newOption = document.createElement("option");
+
+        newOption.value = name.name;
+
+     
+        newOption.textContent = name.name;
+
+        mountainSelection.appendChild(newOption);
+
+
+    })
+
+
+
+
+
+}
+function displayMountainCard(event) {
+
+   
+    let mountainDiv = document.querySelector("#listOfMountains");
+
+    mountainDiv.innerHTML = "";
+
+
+    let selectedMountain = event.target.value;
+
+    let mountain = mountainsArray.find((mountain) => {
+
+        if (mountain.name === selectedMountain) {
+            return true;
+        }
+        return false;
+
+    });
+
+
+ let wholeCard = document.createElement("div");
+ wholeCard.classList.add("card", "text-center", "w-25", "mx-auto","mt-4");
+
+ let cardHeader = document.createElement("div");
+ cardHeader.classList.add("card-header", "bg-dark", "text-light");
+
+ cardHeader.innerText = mountain.name;
+
+ let cardBody = document.createElement("div")
+ cardBody.classList.add("card-body","bg-dark", "text-light");
+
+ let cardImage = document.createElement("img");
+ cardImage.classList.add("card-img-top","card-img-fit");
+
+
+ cardImage.src = `images/${mountain.img}`;
+
+
+    let cardText = document.createElement("p");
+   cardText.classList.add("card-text");
+   cardText.innerText = `${mountain.desc}`
+
+cardBody.appendChild(cardImage);
+cardBody.appendChild(cardText);
+
+let footer1= document.createElement("div")
+footer1.classList.add("card-footer", "bg-secondary");
+
+footer1.innerText = `Elevation : ${mountain.elevation}`;
+
+let footer2= document.createElement("div")
+footer2.classList.add("card-footer", "bg-secondary");
+
+footer2.innerText = `Effort : ${mountain.effort}`;
+
+wholeCard.appendChild(cardHeader);
+wholeCard.appendChild(cardBody);
+wholeCard.appendChild(footer1);
+wholeCard.appendChild(footer2);
+
+mountainDiv.appendChild(wholeCard);
 
 
 }
--- scripts/national_parks.js
+++ scripts/national_parks.js
@@ -1,47 +1,60 @@
 "use strict"
 
-
 window.onload = () => {
 
     initParkselect();
+
+    initTypeSelect();
+
+
 
     
     let selectByLocation = document.querySelector("#selectPark");
 
     let selectByType = document.querySelector("#selectType");
 
+    let locationButton = document.querySelector("#location");
+
+    let parkTypeButton = document.querySelector("#type");
+
    
-    selectByLocation.addEventListener("change", getInfoList)
+    selectByLocation.addEventListener("change", getInfoOffList);
+
+    selectByType.addEventListener("change", gettypeOffList);
+
+
 
 }
-function getInfoList(event) {
+function getInfoOffList(event) {
 
+   
     let selectedState = event.target.value;
 
     
     let matchingActivities = nationalParksArray.filter((park) => {
 
+      
         return park.State === selectedState;
 
 
 
     })
-   
-    let tableBody = document.querySelector("#inforTableBody");
     
+    let tableBody = document.querySelector("#infoTableBody");
+   
     tableBody.innerHTML = "";
 
     matchingActivities.forEach((park) => {
 
-       
         buildTableRow(tableBody, park);
 
 
     })
 
-   
+
     function buildTableRow(tableBody, data) {
 
+      
         let newRow = tableBody.insertRow(-1);
 
         let cell1 = newRow.insertCell(0);
@@ -57,41 +70,42 @@ function getInfoList(event) {
         let cell4 = newRow.insertCell(3)
         cell4.innerHTML = `Phone: ${data.Phone}, Fax: ${data.Fax}`
 
+       
         let cell5 = newRow.insertCell(4)
-        if (typeof data.Visit === "undefined"){
+        if (!data.Visit) {
             cell5.innerHTML = "N/A"
-        }else{
+        } else {
             cell5.innerHTML = data.Visit;
         }
     }
 
 }
 
-
 function initParkselect() {
 
-    let selectByLocation = document.querySelector("#selectPark");
    
+    let selectByLocation = document.querySelector("#selectPark");
+
     let defaultOption = document.createElement("option");
 
-    defaultOption.value = ""
+ 
+    defaultOption.value = "";
 
-    
+    selectByLocation.length = 0;
+
     defaultOption.textContent = "-- Choose Park Location--";
 
-   
     selectByLocation.appendChild(defaultOption);
 
-    
+   
     locationsArray.forEach((state) => {
 
-       
+      
         let newOption = document.createElement("option");
 
-     
         newOption.value = state;
 
-     
+      
         newOption.textContent = state;
 
         selectByLocation.appendChild(newOption);
@@ -103,13 +117,12 @@ function initParkselect() {
 }
 
 function matchByStateName(nationalParksArray, state) {
-
     
     let matching = [];
-   
+  
     let numItems = nationalParksArray.length;
 
-  
+
     for (let i = 0; i < numItems; i++) {
         if (nationalParksArray[i].State === state) {
            
@@ -117,5 +130,102 @@ function matchByStateName(nationalParksArray, state) {
         }
     }
 
+  
     return matching;
+}
+
+
+function initTypeSelect() {
+
+ 
+    let selectByType = document.querySelector("#selectType");
+   
+    let defaultOption = document.createElement("option");
+
+    defaultOption.textContent = "-- Choose A Park Type--";
+
+    defaultOption.value = "0";
+
+
+    selectByType.appendChild(defaultOption);
+
+   
+    parkTypesArray.forEach((park) => {
+
+        let newOption = document.createElement("option");
+
+     
+        newOption.value = park;
+
+     
+        newOption.textContent = park;
+
+        selectByType.appendChild(newOption);
+
+
+    })
+}
+function gettypeOffList(event) {
+
+
+    let selectedType = event.target.value;
+
+    let matchingTypes = nationalParksArray.filter((type) => {
+
+        for (let i = 0; i < parkTypesArray.length; i++) {
+          
+            if (type.LocationName.indexOf(selectedType) !== -1) {
+
+                return true
+            }
+            return false
+        };
+
+
+
+    });
+
+    let tableBody = document.querySelector("#infoTableBody");
+  
+    tableBody.innerHTML = "";
+ 
+    let selectByType = document.querySelector("#selectType");
+
+
+
+    matchingTypes.forEach((type) => {
+
+       
+        buildTableRow(tableBody, type);
+
+    })
+}
+
+function buildTableRow(tableBody, chosen) {
+
+    let newRow = tableBody.insertRow(-1);
+
+    let cell1 = newRow.insertCell(0);
+   
+    cell1.innerHTML = chosen.LocationID
+
+    let cell2 = newRow.insertCell(1);
+    cell2.innerHTML = chosen.LocationName
+
+    let cell3 = newRow.insertCell(2);
+    cell3.innerHTML = `${chosen.Address}, ${chosen.City}, ${chosen.State}, ${chosen.ZipCode}`
+
+    let cell4 = newRow.insertCell(3)
+    cell4.innerHTML = `Phone: ${chosen.Phone}, Fax: ${chosen.Fax}`
+
+
+    let cell5 = newRow.insertCell(4)
+    if (!chosen.Visit) {
+        cell5.innerHTML = "N/A"
+    } else {
+        cell5.innerHTML = chosen.Visit;
+
+
+
+    }
 }
